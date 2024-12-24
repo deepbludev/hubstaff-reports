e76b8f3f2052d7a3f871a7d3040b3589476ab9b3
Carlo Casorzo <carlo@deepblu.dev>
2024-12-24 12:54:15 +0100

    refactor: improve startup and shutdown

--- app/main.py
+++ app/main.py
@@ -32,11 +32,13 @@ async def health_check():
 async def startup(app: FastAPI):
     """
     Handles the startup of the application.
-    It initializes the cron job scheduler.
+    It initializes the cron job scheduler and registers the report jobs.
     """
-    scheduler = get_scheduler()
+    app.state.scheduler = (scheduler := get_scheduler())
+
     register_report_jobs(scheduler)
     scheduler.start()
+
     logger.info(f"Scheduled jobs: {len(scheduler.get_scheduled_jobs())}")
     for job in scheduler.get_scheduled_jobs():
         logger.info(f"Job ({job.id}): {job}")
@@ -47,5 +49,7 @@ async def shutdown(app: FastAPI):
     Handles the shutdown of the application.
     It makes sure that the cron jobs are stopped.
     """
-    scheduler = get_scheduler()
-    scheduler.shutdown()
+    logger.info("Shutting down Job scheduler...")
+    app.state.scheduler.shutdown()
+
+    logger.info("Shutdown complete")
